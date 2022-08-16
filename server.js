1bab4d171853b60423c0f2b0b20b6c2ed79928a4
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-08-16 17:26:41 +0200

    cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const cors = require('cors')
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 8080;
@@ -12,32 +13,15 @@ connectDB();
 const app = express();
 
 app.use(express.json())
+
+app.use(cors({ credentials: true, origin: 'https://reactredux-todoapp-withauth-fe.netlify.app' })) // bunun sonunda / olmayacak
+
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/todos', require('./routes/todoRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
 
-// app.use('/api/auth', authRoutes)
-// app.use('/api/todos', todoRoutes) // todoRoutes icindeki her route'a burdan gidiyor
 
-// Serve frontend //
-// if (process.env.NODE_ENV === 'production') {
-//     // app.use(express.static(path.join(__dirname, '../frontend/build')));
-//     app.use(express.static('frontend/build'));
-
-//     app.get('*', (req, res) =>
-//         res.sendFile(
-//             path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-//         )
-//     );
-// } else {
-//     app.get('/', (req, res) => res.send('Please set to production'));
-// }
-// app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
-// });
 
 app.use(errorHandler);
 
